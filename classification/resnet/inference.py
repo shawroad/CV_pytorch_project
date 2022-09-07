b029613b25e663e1ad31b378e97d4b8ae24b0ee3
shawroad <luxiaonlp@163.com>
2022-09-07 18:26:46 +0800

    infer

---
+++ classification/resnet/inference.py
@@ -0,0 +1,73 @@
+"""
+@file   : inference.py
+@author : xiaolu
+@email  : luxiaonlp@163.com
+@time   : 2022-09-07
+"""
+import os
+import json
+
+import torch
+from PIL import Image
+from torchvision import transforms
+import matplotlib.pyplot as plt
+
+from model import resnet34
+
+
+def main():
+    data_transform = transforms.Compose(
+        [transforms.Resize(256),
+         transforms.CenterCrop(224),
+         transforms.ToTensor(),
+         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+
+    # load image
+    img_path = "../../../../data/flower_data/flower_photos/tulips/112334842_3ecf7585dd.jpg"
+    assert os.path.exists(img_path), "file: '{}' dose not exist.".format(img_path)
+    img = Image.open(img_path)
+    plt.imshow(img)
+    # [N, C, H, W]
+    img = data_transform(img)
+    # expand batch dimension
+    img = torch.unsqueeze(img, dim=0)
+
+    # read class_indict
+    json_path = './output/class_indices.json'
+    assert os.path.exists(json_path), "file: '{}' dose not exist.".format(json_path)
+
+    with open(json_path, "r") as f:
+        class_indict = json.load(f)
+
+    # create model
+    model = resnet34(num_classes=5)
+
+    # load model weights
+    weights_path = "./output/best_model.bin"
+    assert os.path.exists(weights_path), "file: '{}' dose not exist.".format(weights_path)
+    model.load_state_dict(torch.load(weights_path))
+
+    if torch.cuda.is_available():
+        model.cuda()
+
+    # prediction
+    model.eval()
+    with torch.no_grad():
+        # predict class
+        if torch.cuda.is_available():
+            img = img.cuda()
+        output = torch.squeeze(model(img)).cpu()
+        predict = torch.softmax(output, dim=0)
+        predict_cla = torch.argmax(predict).numpy()
+
+    print_res = "class: {}   prob: {:.3}".format(class_indict[str(predict_cla)],
+                                                 predict[predict_cla].numpy())
+    plt.title(print_res)
+    for i in range(len(predict)):
+        print("class: {:10}   prob: {:.3}".format(class_indict[str(i)],
+                                                  predict[i].numpy()))
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
