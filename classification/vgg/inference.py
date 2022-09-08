50793fb4df941d4c25e746d0bb2de2cb3e6abe34
shawroad <luxiaonlp@163.com>
2022-09-08 14:59:41 +0800

    vgg

---
+++ classification/vgg/inference.py
@@ -0,0 +1,69 @@
+"""
+@file   : inference.py
+@author : xiaolu
+@email  : luxiaonlp@163.com
+@time   : 2022-09-08
+"""
+import os
+import json
+
+import torch
+from PIL import Image
+from torchvision import transforms
+import matplotlib.pyplot as plt
+
+from model import vgg
+
+
+def main():
+    data_transform = transforms.Compose(
+        [transforms.Resize((224, 224)),
+         transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    # load image
+    img_path = "../../../../data/flower_data/flower_photos/tulips/112334842_3ecf7585dd.jpg"
+    assert os.path.exists(img_path), "file: '{}' dose not exist.".format(img_path)
+    img = Image.open(img_path)
+    plt.imshow(img)
+    # [N, C, H, W]
+    img = data_transform(img)
+    # expand batch dimension
+    img = torch.unsqueeze(img, dim=0)
+
+    # read class_indict
+    json_path = './output/class_indices.json'
+    assert os.path.exists(json_path), "file: '{}' dose not exist.".format(json_path)
+
+    with open(json_path, "r") as f:
+        class_indict = json.load(f)
+
+    # create model
+    model = vgg(model_name="vgg16", num_classes=5)
+
+    weights_path = "./vgg16Net.pth"
+    assert os.path.exists(weights_path), "file: '{}' dose not exist.".format(weights_path)
+    model.load_state_dict(torch.load(weights_path, map_location='cpu'))
+    if torch.cuda.is_available():
+        model.cuda()
+
+    model.eval()
+    with torch.no_grad():
+        # predict class
+        if torch.cuda.is_available():
+            img = img.cuda()
+        output = torch.squeeze(model(img)).cpu()
+        predict = torch.softmax(output, dim=0)
+        predict_cla = torch.argmax(predict).numpy()
+
+    print_res = "class: {}   prob: {:.3}".format(class_indict[str(predict_cla)],
+                                                 predict[predict_cla].numpy())
+    plt.title(print_res)
+    for i in range(len(predict)):
+        print("class: {:10}   prob: {:.3}".format(class_indict[str(i)],
+                                                  predict[i].numpy()))
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
